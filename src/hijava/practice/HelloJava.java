e3b8971f0d68690bc55b365553a063c7d3e26734
ian807b <ian807b@gmail.com>
2022-10-27 09:40:52 -0700

    Eclipse test

---
+++ src/hijava/practice/HelloJava.java
@@ -0,0 +1,10 @@
+package hijava.practice;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ianh
+ *
+ */
+module hijava {
+}
