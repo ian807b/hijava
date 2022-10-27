e306e0612457b561739adbaeb486e2c0906fa2ed
ian807b <ian807b@gmail.com>
2022-10-27 10:02:28 -0700

    Add swap function

--- src/hijava/practice/HelloJava.java
+++ src/hijava/practice/HelloJava.java
@@ -1,10 +1,21 @@
 package hijava.practice;
 
 public class HelloJava {
-
+	int x = 10;
+	int y = 20;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		HelloJava hj = new HelloJava();
+		System.out.println("x = " + hj.x);
+		System.out.println("x = " + hj.y);
+		swap(hj);
+		System.out.println("x = " + hj.x);
+		System.out.println("x = " + hj.y);
 	}
 
+	public static void swap(HelloJava hj) {
+		int temp = hj.x;
+		hj.x = hj.y;
+		hj.y = temp;
+	}
 }
